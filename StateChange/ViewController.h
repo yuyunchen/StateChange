46b60b92f46b356e8c42c1cded06ba799cbf904f
yuyunchen <cyy9119@cyy9119s-iMac.local>
2017-03-04 22:03:43 +0800

    Initial Commit

---
+++ StateChange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StateChange
+//
+//  Created by cyy9119 on 3/4/17.
+//  Copyright © 2017 everflag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StateChange/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StateChange
+//
+//  Created by cyy9119 on 3/4/17.
+//  Copyright © 2017 everflag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
